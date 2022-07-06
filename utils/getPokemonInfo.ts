93e8200fcb10953a1117c121e67f815ccc854973
fzarate <fede.zarate.uni@gmail.com>
2022-07-05 23:43:50 -0500

    Incremental Static Generation

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,23 +2,27 @@ import { pokeApi } from "../api";
 import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-  const { id, name, sprites } = data;
+    const { id, name, sprites } = data;
 
-  return {
-    id,
-    name,
-    sprites: {
-      front_default: sprites.front_default,
-      back_default: sprites.back_default,
-      front_shiny: sprites.front_shiny,
-      back_shiny: sprites.back_shiny,
-      other: {
-        dream_world: {
-          front_default: sprites?.other?.dream_world?.front_default,
+    return {
+      id,
+      name,
+      sprites: {
+        front_default: sprites.front_default,
+        back_default: sprites.back_default,
+        front_shiny: sprites.front_shiny,
+        back_shiny: sprites.back_shiny,
+        other: {
+          dream_world: {
+            front_default: sprites?.other?.dream_world?.front_default,
+          },
         },
       },
-    },
-  };
+    };
+  } catch (error) {
+    return null
+  }
 }
